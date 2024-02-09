1007be2d9191c7a59f6230eff0b8f9baefa8ce8b
Lukas Larndorfer <l.larndorfer@students.htl-leonding.ac.at>
2024-02-09 09:02:38 +0100

    final Adjustments + fixed Index with -1

--- starter/TrainSwitching.Logic/SwitchingOperationParser.cs
+++ starter/TrainSwitching.Logic/SwitchingOperationParser.cs
@@ -9,7 +9,69 @@ public static class SwitchingOperationParser
     /// <returns>The parsed switching operation</returns>
     public static SwitchingOperation Parse(string inputLine)
     {
-        // TODO: Implement this method
-        throw new NotImplementedException();
+        SwitchingOperation mySwitch = new SwitchingOperation();
+
+        var splittedString = inputLine.Split(' ');
+        mySwitch.TrackNumber = int.Parse(splittedString[2].Remove(splittedString[2].Length - 1));
+
+
+        if (inputLine.Contains("add"))
+        {
+            mySwitch.OperationType = 1;
+        }
+        else if (inputLine.Contains("remove"))
+        {
+            mySwitch.OperationType = -1;
+        }
+        else if (inputLine.Contains("train leaves"))
+        {
+            mySwitch.OperationType = 0;
+        }
+
+        if (inputLine.Contains("West"))
+        {
+            mySwitch.Direction = 1;
+        }
+        else if (inputLine.Contains("East"))
+        {
+            mySwitch.Direction = 0;
+        }
+
+
+        if (inputLine.Contains("Passenger Wagon"))
+        {
+            mySwitch.WagonType = 0;
+        }
+        else if (inputLine.Contains("Locomotive"))
+        {
+            mySwitch.WagonType = 1;
+        }
+        else if (inputLine.Contains("Freight Wagon"))
+        {
+            mySwitch.WagonType = 2;
+        }
+        else if (inputLine.Contains("Car Transport Wagon"))
+        {
+            mySwitch.WagonType = 3;
+        }
+        else
+        {
+            mySwitch.WagonType = null;
+        }
+
+        var halfText = inputLine.Split(", ");
+        var newString = halfText[1].Split(' ');
+        if (int.TryParse(newString[1], out int myNum))
+        {
+            mySwitch.NumberOfWagons = myNum;
+        }
+        else
+        {
+            mySwitch.NumberOfWagons = null;
+        }
+
+        return mySwitch;
     }
 }
+
+
--- starter/TrainSwitching.Logic/TrainStation.cs
+++ starter/TrainSwitching.Logic/TrainStation.cs
@@ -1,13 +1,29 @@
 namespace TrainSwitching.Logic;
 
+/*     public const int OPERATION_ADD = 1;
+    public const int OPERATION_TRAIN_LEAVE = 0;
+    public const int OPERATION_REMOVE = -1;
+
+    public const int WAGON_TYPE_PASSENGER = 0;
+    public const int WAGON_TYPE_LOCOMOTIVE = 1;
+    public const int WAGON_TYPE_FREIGHT = 2;
+    public const int WAGON_TYPE_CAR_TRANSPORT = 3;
+
+    public const int DIRECTION_EAST = 0;
+    public const int DIRECTION_WEST = 1; */
+
 public class TrainStation
 {
     public Track[] Tracks { get; }
 
     public TrainStation()
     {
-        // TODO: Implement this method
-        throw new NotImplementedException();
+        Tracks = new Track[10];
+
+        for (var i = 0; i < Tracks.Length; i++)
+        {
+            Tracks[i] = new Track();
+        }
     }
 
     /// <summary>
@@ -17,8 +33,71 @@ public class TrainStation
     /// <returns>Returns true if the operation could be applied, otherwise false</returns>
     public bool TryApplyOperation(SwitchingOperation op)
     {
-        // TODO: Implement this method
-        throw new NotImplementedException();
+        if (op.TrackNumber > 10 || op.TrackNumber < 1) //* Adding or removing wagons to/from tracks that do not exist.
+        {
+            return false;
+        }
+
+        if (op.TrackNumber > 8 && op.Direction == 0)
+        {
+            return false;
+        }
+        if (Tracks[op.TrackNumber - 1].Wagons.Count < op.NumberOfWagons)
+        {
+            return false;
+        }
+
+
+
+        if (op.OperationType == 1) // ADD
+        {
+            if (op.Direction == 0) // East
+            {
+                Tracks[op.TrackNumber - 1].Wagons.Insert(0, (int)op.WagonType!);  // East vorne adden | West hinten adden 
+            }
+            else
+            {
+                Tracks[op.TrackNumber - 1].Wagons.Add((int)op.WagonType!);
+            }
+        }
+        if (op.OperationType == 0) // LEAVE
+        {
+
+
+            if (Tracks[op.TrackNumber - 1].Wagons.Any() && Tracks[op.TrackNumber - 1].Wagons.Contains(1))
+            {
+                Tracks[op.TrackNumber - 1].Wagons.Clear();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        if (op.OperationType == -1)
+        {
+
+            if (op.Direction == 0) // EAST
+            {
+                for (int i = 0; i < op.NumberOfWagons; i++)
+                {
+                    Tracks[op.TrackNumber - 1].Wagons.RemoveAt(0);
+                }
+
+            }
+            else
+            {
+                for (int i = 0; i < op.NumberOfWagons; i++)
+                {
+                    Tracks[op.TrackNumber - 1].Wagons.RemoveAt(Tracks[op.TrackNumber - 1].Wagons.Count - 1);
+                }
+            }
+        }
+
+
+        return true;
     }
 
     /// <summary>
@@ -30,7 +109,32 @@ public class TrainStation
     /// </remarks>
     public int CalculateChecksum()
     {
-        // TODO: Implement this method
-        throw new NotImplementedException();
+        var totalsum = 0;
+        for (var i = 0; i < Tracks.Length; i++) //foreach Track
+        {
+            int shortsum = 0;
+            foreach (var w in Tracks[i].Wagons)
+            {
+                switch (w)
+                {
+                    case 0: // PASSENGER
+                        shortsum += 1;
+                        break;
+
+                    case 1: // LOCOMOTIVE
+                        shortsum += 10;
+                        break;
+
+                    case 2: // FREIGHT
+                        shortsum += 20;
+                        break;
+                    case 3: // CAR TRANSPORT WAGON
+                        shortsum += 30;
+                        break;
+                }
+            }
+            totalsum += shortsum * (i + 1);
+        }
+        return totalsum;
     }
 }
